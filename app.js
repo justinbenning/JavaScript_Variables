4215e77db0e9f29f384537aff5e80a614e1b049a
JustinBenning <justin.benning@gmail.com>
2024-02-21 09:53:10 -0500

    upload of exercise

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName= "j";
+let lastName;
+var age;
 
 // Exercise 2
+firstName = "Justin";
+lastName = "Benning";
+age = 35;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let seletectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
